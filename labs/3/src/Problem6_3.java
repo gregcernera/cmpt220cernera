4c3b3ce21e309493ae1739024ee2b26a4b5e00aa
gregcernera <Gregory.Cernera1@marist.edu>
2017-02-07 19:05:28 -0500

    indentation fix

--- labs/3/src/Problem6_3.java
+++ labs/3/src/Problem6_3.java
@@ -7,7 +7,7 @@ public class Problem6_3 {
 	
 	// Return the reversal of an integer, i.e., reverse(456) returns 654
 	public static int reverse(int number) {
-		int newNumber = 0;
+	    int newNumber = 0;
 	    while (number > 0) {
 	    	int digit = number % 10;
 	    	newNumber = (newNumber * 10) + digit;
@@ -18,23 +18,23 @@ public class Problem6_3 {
 	
 	// Return true if number is palindrome
 	public static boolean isPalindrome(int number) {
-		if (reverse(number) == number) {
-			return true;
-		} else {
-			return false;
-		}
+	    if (reverse(number) == number) {
+		return true;
+	    } else {
+		return false;
+	    }
 	}
 	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter an integer: ");
-		int n = input.nextInt();
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter an integer: ");
+	    int n = input.nextInt();
 		
-		if (isPalindrome(n) == true) {
-			System.out.println("The number " + n + " is a palindrome");
-		} else {
-			System.out.println("The number " + n + " is not a palindrome");
-		}
+	    if (isPalindrome(n) == true) {
+		System.out.println("The number " + n + " is a palindrome");
+	    } else {
+		System.out.println("The number " + n + " is not a palindrome");
+	    }
 		
 	}
 
